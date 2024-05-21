06bffa8be61c33c9116a479a6ec9aaf5bc058dfe
Legonois <74224353+Legonois@users.noreply.github.com>
2024-05-21 16:34:03 -0600

    Abstracted a lot of the logic to the menu file

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,6 @@ use std::io::{self, Write, Read};
 // modules
 mod menu;
 
-// exit codes:
-// 1 | Keyboard Interrupt
-
 fn terminal_preferences() {
     enable_raw_mode().expect("Failed to enable raw mode");
     io::stdout().execute(terminal::EnterAlternateScreen).unwrap();
@@ -34,10 +31,6 @@ fn wait_until_key_pressed() {
 
 fn get_args() -> Vec<String> {
     return env::args().collect();
-
-    // for (index, arg) in args.iter().enumerate() {
-    //     println!("Arguement[{}]: {}", index, arg);
-    // }
 }
 
 struct TerminalCleanUp;
@@ -46,7 +39,8 @@ impl Drop for TerminalCleanUp {
     // Gets called when the instance goes out of scope
     // In this implementation, when main is out of scope
     fn drop(&mut self) {
-        println!("wiah");
+        println!("Uh oh! The tool crashed!");
+        wait_until_key_pressed();
         terminal_defaults();
     }
 }
@@ -54,92 +48,13 @@ impl Drop for TerminalCleanUp {
 fn main() {
     let _clean_up = TerminalCleanUp;
     terminal_preferences();
-    println!("Enter Password");
-    wait_until_key_pressed();
 
-    let mut input = String::new();
+    let password = menu::get_password();
 
-    // let mut interation: u32 = 0;
-
-    loop {
-        if event::poll(std::time::Duration::from_millis(500)).unwrap() {
-            if let Event::Key(event) = event::read().expect("Failed to read line") {
-
-                // Only account for key presses
-                if event.kind != KeyEventKind::Press {
-                    continue;
-                }
-
-                let mut char_input: Option<char> = None;
-
-                match event.code {
-                    // Exit if control + C is pressed
-                    KeyCode::Char(c) if event.modifiers.contains(KeyModifiers::CONTROL) && c == 'c' => {
-                        panic!("Keyboard Interrupt | Exiting");
-                    }
-    
-                    KeyCode::Enter => {
-                        break;
-                    }
-    
-                    // Output inputted char
-                    KeyCode::Char(c) => {
-                        char_input = Some(c);
-                    }
-
-                    _ => {
-                        // continue;
-                    }
-                }
-
-                if let Some(char) = char_input {
-                    // interation += 1;
-
-                    print!("{}\n", char);
-                    input.push(char);
-                }
-
-            }
-        }
-    }
-
-    // loop {
-    //     io::stdout().flush().unwrap();
-
-    //     if event::poll(std::time::Duration::from_millis(500)).unwrap() {
-
-    //         if let Event::Key(key_event) = event::read().unwrap() {
-
-    //             println!("{}", iterations);
-
-    //             match key_event.code {
-    //                KeyCode::Char(c) if key_event.modifiers.contains(KeyModifiers::CONTROL) && c == 'c' => {
-    //                    panic!("Keyboard Interrupt | Exiting");
-    //                }
-    //                 KeyCode::Char(c) => {
-    //                     print!("{}", c);
-    //                     io::stdout().flush().unwrap();
-    //                     input.push(c);
-    //                 }
-    //                 KeyCode::Enter => {
-    //                     break;
-    //                 }
-    //                 KeyCode::F(2) => {
-    //                     break;
-    //                 }
-    //                 // Default case
-    //                 _ => {} // ignore other keys
-    //             }
-
-    //             iterations += 1;
-    //         }
-    //     }
-    // }
-
-    println!("You entered: {}", input);
+    println!("You entered: {}", password);
 
     let help: String = menu::help_menu();
     println!("{}", help);
 
-    wait_until_key_pressed();
+    menu::initialize_tui();
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,3 +1,87 @@
+use std::{char, env};
+// use std::io;
+// use std::io::Read;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use crossterm::terminal::{self, disable_raw_mode, enable_raw_mode};
+use crossterm::cursor;
+use crossterm::ExecutableCommand;
+use std::io::{self, stdout, Read, Write};
+
+struct Position {
+    x: u16,
+    y: u16,
+}
+
+impl Position {
+    fn new(x: u16, y: u16) -> Position {
+        return Position { x, y };
+    }
+
+    fn zeros() -> Position {
+        return Position { x: 0, y: 0 };
+    }
+}
+
+enum CursorStyle {
+    Blinking,
+    Solid
+}
+
+struct Cursor {
+    position: Position,
+    style: CursorStyle
+}
+
+impl Cursor {
+
+    fn new() -> Cursor {
+        return Cursor { position: Position::zeros(), style: CursorStyle::Solid };
+    }
+
+    fn set_style(&mut self, style: CursorStyle) {
+
+        match style {
+            CursorStyle::Blinking => {
+                io::stdout().execute(cursor::EnableBlinking).unwrap();
+            }
+            CursorStyle::Solid => {
+                io::stdout().execute(cursor::DisableBlinking).unwrap();
+            }
+        }
+
+        self.style = style;
+    }
+    fn set_position(&mut self, position: Position) {
+        self.position = position;
+    } 
+}
+
+struct Terminal {
+    cursor: Cursor,
+    size: (u16, u16),
+}
+
+impl Terminal {
+    fn new() -> Terminal {
+        let terminal =  Terminal {
+            cursor: Cursor::new(),
+            size: (0, 0)
+        };
+
+        return terminal;
+    }
+
+    fn set_size(&mut self) {
+        let window_size = terminal::window_size().expect("Error finding window size");
+        self.size = (window_size.width, window_size.height)
+    }
+}
+
+pub fn initialize_tui() {
+    Terminal::new();
+}
+
+
 pub fn help_menu() -> String {
 
     let help_menu = String::from(
@@ -29,4 +113,131 @@ pub fn about_menu() -> String {
     );
 
     about_menu
+}
+
+fn get_text_consume_input() -> String {
+    return get_text(true);
+}
+
+fn get_text(consume_input: bool) -> String {
+
+    let mut output = String::new();
+
+    loop {
+        if event::poll(std::time::Duration::from_millis(500)).unwrap() {
+
+            if let Event::Key(event) = event::read().expect("Failed to read line") {
+
+
+                // Only account for key presses
+                if event.kind != KeyEventKind::Press {
+                    continue;
+                }
+
+                match event.code {
+                    // Exit if control + C is pressed
+                    KeyCode::Char(c) if event.modifiers.contains(KeyModifiers::CONTROL) && c == 'c' => {
+                        panic!("Keyboard Interrupt | Exiting");
+                    }
+    
+                    KeyCode::Enter => {
+                        print_char('\n');
+                        break;
+                    }
+    
+                    // Output inputted char
+                    KeyCode::Char(c) => {
+                        if !consume_input {
+                            print_char(c);
+                        }
+                        output.push(c);
+                    }
+
+                    KeyCode::Backspace => {
+                        if !consume_input {
+                            remove_char();
+                        }
+                        output.pop();
+                    }
+
+                    _ => {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    return output;
+}
+
+fn get_cursor_position() -> Position {
+    let position: Position;
+
+    match cursor::position() {
+        Ok((x, y)) => {
+            position = Position::new(x, y);
+            // println!("The current cursor position is: ({}, {})", x, y);
+        }
+        Err(e) => {
+            panic!("Failed to get the cursor position: {}", e);
+        }
+    }
+
+    return position;
+}
+
+fn move_cursor(position: Position) {
+    stdout().execute(cursor::MoveTo(position.x, position.y)).unwrap();
+}
+
+fn print_char(c: char) {
+
+    print!("{}", c);
+
+    if c == '\n' {
+        let mut position = get_cursor_position();
+        position.x = 0;
+
+        move_cursor(position);
+    } else {
+        increment_cursor();
+    }
+}
+
+fn increment_cursor() {
+    let mut position = get_cursor_position();
+
+    position.x += 1;
+    move_cursor(position);
+}
+
+fn decrement_cursor() {
+    let mut position = get_cursor_position();
+
+    if position.x - 1 == 0 {
+        return;
+    }
+
+    position.x -= 1;
+    move_cursor(position);
+}
+
+fn remove_char() {
+    decrement_cursor();
+    print!(" ");
+    decrement_cursor();
+}
+
+
+
+
+
+pub fn get_password() -> String {
+
+    println!("Enter Password");
+
+    let password = get_text(false);
+
+    return password;
 }
